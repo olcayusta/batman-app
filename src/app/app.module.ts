8fb95a80d60879f820c416ed9f989dfbe3214b63
olcay <olcaymaster@gmail.com>
2022-03-02 01:01:09 +0300

    hero component added and material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { LazyDirective } from './lazy.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LazyDirective
+    LazyDirective,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,8 @@ import { LazyDirective } from './lazy.directive';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
