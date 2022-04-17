2e9b438a8063b52fd764b38476167de90f4ea19d
olcay <olcayusta02@gmail.com>
2022-04-18 01:36:47 +0300

    update share api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,16 @@ export class AppComponent implements OnInit {
             alert('Available update!')
         })
     }
+
+    share() {
+        if (navigator.share) {
+            navigator.share({
+                title: 'web.dev',
+                text: 'Check out web.dev.',
+                url: 'https://web.dev/',
+            })
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.log('Error sharing', error));
+        }
+    }
 }
